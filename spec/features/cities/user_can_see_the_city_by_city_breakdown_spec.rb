d47c8811f38d3c34f68a675b0b9f5d7353bf3ba6
nergdvnlt <tylerlundgren@yahoo.com>
2018-09-27 13:52:11 -0600

    Using BDD to start formulating how I want the page to look. Will narrow and refine in further iterations.

---
+++ spec/features/cities/user_can_see_the_city_by_city_breakdown_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'User visits the root page' do
+  before :each do
+    # Populate database with seed information
+    @city_list = ['Boston', 'Boulder', 'Chicago', 'Denver', 'Los Angeles', 'New York', 'San Francisco']
+    @city_list.each do |city_name|
+      City.create!(name: city_name)
+    end
+  end
+
+  describe 'to see a breakdown of the cities' do
+    it 'has all the cities listed' do
+      # User visits the root page
+      visit '/'
+
+      # The page has all the cities listed to start
+      @city_list.each do |city_name|
+        expect(page).to have_content(city_name.name)
+      end
+    end
+  end
+end
