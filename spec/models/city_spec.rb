3c12f54f7d2ac59b172e038b540b958955f430a4
nergdvnlt <tylerlundgren@yahoo.com>
2018-09-27 13:35:42 -0600

    Adding relationship between city and language. Normally I would have the relationship be many to many, but for the specific implementation I am working towards I want to keep the individual languages per city separate. Further iterations or need for other features could change this.

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe City, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+
+  describe 'relationships' do
+    it { should have_many :languages }
+  end
 end
--- spec/models/language_spec.rb
+++ spec/models/language_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Language, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
   end
+
+  describe 'relationships' do
+    it { should belong_to :city }
+  end
 end
