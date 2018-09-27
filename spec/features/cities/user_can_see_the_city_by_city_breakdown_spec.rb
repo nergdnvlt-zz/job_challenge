a78fdd0a99db788a36f2fd4eee853a978cdf4203
nergdvnlt <tylerlundgren@yahoo.com>
2018-09-27 14:15:06 -0600

    Building the basic page layout with the database object, now about to start API interaction.

--- spec/features/cities/user_can_see_the_city_by_city_breakdown_spec.rb
+++ spec/features/cities/user_can_see_the_city_by_city_breakdown_spec.rb
@@ -25,4 +25,19 @@ describe 'User visits the root page' do
       end
     end
   end
+
+  describe 'to see a breakdown of languages for each city' do
+    it 'has all the languages for each city listed' do
+      # User visits the root page
+      visit '/'
+
+      # The page has all the cities listed
+      @language_list.each do |language|
+        expect(page).to have_content(language)
+      end
+      expect(page).to have_content('Full Time')
+      expect(page).to have_content('Part Time')
+      expect(page).to have_content('Unknown')
+    end
+  end
 end
