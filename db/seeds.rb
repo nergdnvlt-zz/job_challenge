2fd33c6286ed7c26b7ede44854af1c7a32e0c86b
nergdvnlt <tylerlundgren@yahoo.com>
2018-09-27 20:07:10 -0600

    Still changing the interaction pattern from earlier.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 city_list = ['Boston', 'Boulder', 'Chicago', 'Denver', 'Los Angeles', 'New York', 'San Francisco']
-language_list = ['Go', 'JavaScript', 'Python', 'Ruby', 'Scala', 'Other']
 
 city_list.each do |city_name|
-  city = City.create!(name: city_name)
-  language_list.each do |language|
-    city.languages.create!(name: language)
-  end
+  City.create!(name: city_name)
 end
